53ba26781ed8e081b0a845606e875f34efa70452
jeanpierrebroz <jeanpierrebroz@gmail.com>
2026-02-09 12:47:41 -0700

    branches and functions

---
+++ functions/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    println!("Hello, world!");
+
+    another_function();
+    yet_another_function(5);
+    print_labeled_measurement(5, 'm');
+}
+
+fn another_function() {
+    println!("Another function.");
+}
+
+fn yet_another_function(x: i32) {
+    println!("The value of x is: {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
