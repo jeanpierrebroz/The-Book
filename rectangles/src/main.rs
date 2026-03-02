77b9ae54bc95f16730dd041d65542003f2ad5a4b
jeanpierrebroz <jeanpierrebroz@gmail.com>
2026-03-01 21:08:43 -0700

    Update main.rs

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,15 +4,20 @@ struct Rectangle {
     height: u32
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {
         width: 50,
         height: 30
     };
     
-    println!("rect1 is {rect1:#?}");
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    println!(
+            "The area of the rectangle is {} square pixels.",
+            rect1.area()
+        );
 }
