a0d0d1a45c07a9b3aa0e65337d567e208e82ae46
jeanpierrebroz <jeanpierrebroz@gmail.com>
2026-03-01 21:00:49 -0700

    add struct

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,9 +1,17 @@
-fn main() {
-    let rect1 = (30, 50);
-    
-    println!("The area of the rectangle is {} square pixels.", area(rect1));
+struct Rectangle {
+    width: u32,
+    height: u32
 }
 
-fn area(dim: (u32, u32)) -> u32 {
-    dim.0 * dim.1
+fn main() {
+    let rect1 = Rectangle {
+        width: 50,
+        height: 30
+    };
+    
+    println!("The area of the rectangle is {} square pixels.", area(&rect1));
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
