fc094948f744f3797aa7e1d4ebf378b15566c20a
jeanpierrebroz <jeanpierrebroz@gmail.com>
2026-03-23 12:39:27 -0600

    move into struct and function

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,14 +6,23 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     //TODO: add error handling
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = parse_config(&args);
 
-    println!("Searching for {query}");
-    println!("In file {file_path}");
     
-    let contents = fs::read_to_string(file_path)
+    let contents = fs::read_to_string(config.file_path)
             .expect("Should have been able to read the file");
     
         println!("With text:\n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { query, file_path }
 }
