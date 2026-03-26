6d6af1c790b67093fbac437f9993ddca401dfd13
jeanpierrebroz <jeanpierrebroz@gmail.com>
2026-03-26 12:43:32 -0600

    won't compile

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,19 +1,12 @@
-struct CustomSmartPointer {
-    data: String,
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
 }
 
-impl Drop for CustomSmartPointer {
-    fn drop(&mut self) {
-        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
-    }
-}
+use crate::List::{Cons, Nil};
 
 fn main() {
-    let c = CustomSmartPointer {
-        data: String::from("my stuff"),
-    };
-    let d = CustomSmartPointer {
-        data: String::from("other stuff"),
-    };
-    println!("CustomSmartPointers created.");
+    let a = Cons(5, Box::new(Cons(10, Box::new(Nil))));
+    let b = Cons(3, Box::new(a));
+    let c = Cons(4, Box::new(a));
 }
