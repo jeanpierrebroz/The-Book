cccf79cd491c69742197f6c5918e5cb010f50276
jeanpierrebroz <jeanpierrebroz@gmail.com>
2026-03-27 12:33:51 -0600

    spawning tasks

--- hello_async/src/main.rs
+++ hello_async/src/main.rs
@@ -1,30 +1,17 @@
-use trpl::{Either, Html};
+use std::time::Duration;
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-
     trpl::run(async {
-        let title_fut_1 = page_title(&args[1]);
-        let title_fut_2 = page_title(&args[2]);
+        trpl::spawn_task(async {
+            for i in 1..10 {
+                println!("hi number {i} from the first task!");
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        });
 
-        let (url, maybe_title) =
-            match trpl::race(title_fut_1, title_fut_2).await {
-                Either::Left(left) => left,
-                Either::Right(right) => right,
-            };
-
-        println!("{url} returned first");
-        match maybe_title {
-            Some(title) => println!("Its page title is: '{title}'"),
-            None => println!("Its title could not be parsed."),
+        for i in 1..5 {
+            println!("hi number {i} from the second task!");
+            trpl::sleep(Duration::from_millis(500)).await;
         }
-    })
-}
-
-async fn page_title(url: &str) -> (&str, Option<String>) {
-    let text = trpl::get(url).await.text().await;
-    let title = Html::parse(&text)
-        .select_first("title")
-        .map(|title| title.inner_html());
-    (url, title)
+    });
 }
