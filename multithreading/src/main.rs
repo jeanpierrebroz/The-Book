d558074b29a80549b66bdb5254baad0947ead417
jeanpierrebroz <jeanpierrebroz@gmail.com>
2026-03-27 11:35:33 -0600

    channels

--- multithreading/src/main.rs
+++ multithreading/src/main.rs
@@ -1,18 +1,16 @@
+//mpsc stands for multiple producer, single consumer
+use std::sync::mpsc;
 use std::thread;
-use std::time::Duration;
 
 fn main() {
-    let handle = thread::spawn(|| {
-        for i in 1..10 {
-            println!("hi number {i} from the spawned thread!");
-            thread::sleep(Duration::from_millis(1));
-        }
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("Hi!");
+
+        tx.send(val).unwrap();
     });
 
-    handle.join().unwrap();
-
-    for i in 1..5 {
-        println!("hi number {i} from the main thread!");
-        thread::sleep(Duration::from_millis(1));
-    }
+    let received = rx.recv().unwrap();
+    println!("Got: {received}");
 }
