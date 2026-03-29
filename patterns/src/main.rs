06c11f169d10262dc2d91bb8f8b65922b9b02da8
jeanpierrebroz <jeanpierrebroz@gmail.com>
2026-03-29 16:33:20 -0600

    odd behavior

--- patterns/src/main.rs
+++ patterns/src/main.rs
@@ -1,19 +1,12 @@
 fn main() {
-    let favorite_color: Option<&str> = None;
-    let is_tuesday = false;
-    let age: Result<u8, _> = "34".parse();
+    let x = Some(5);
+    let y = 10;
 
-    if let Some(color) = favorite_color {
-        println!("Using your favorite color, {color}, as the background");
-    } else if is_tuesday {
-        println!("Tuesday is green day!");
-    } else if let Ok(age) = age {
-        if age > 30 {
-            println!("Using purple as the background color");
-        } else {
-            println!("Using orange as the background color");
-        }
-    } else {
-        println!("Using blue as the background color");
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(y) => println!("Matched, y = {y}"),
+        _ => println!("Default case, x = {x:?}"),
     }
+
+    println!("at the end: x = {x:?}, y = {y}");
 }
