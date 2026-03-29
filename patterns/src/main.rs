c1a5fdd59fe82334cbea06784b049ed95b2bfed5
jeanpierrebroz <jeanpierrebroz@gmail.com>
2026-03-29 16:37:17 -0600

    juse use 1 of the fields

--- patterns/src/main.rs
+++ patterns/src/main.rs
@@ -6,7 +6,11 @@ struct Point {
 fn main() {
     let p = Point { x: 0, y: 7 };
 
-    let Point { x, y } = p;
-    assert_eq!(0, x);
-    assert_eq!(7, y);
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {x}"),
+        Point { x: 0, y } => println!("On the y axis at {y}"),
+        Point { x, y } => {
+            println!("On neither axis: ({x}, {y})");
+        }
+    }
 }
