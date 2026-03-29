72071bbf0579af59553beddcb7b0b35492b0f285
jeanpierrebroz <jeanpierrebroz@gmail.com>
2026-03-29 16:53:42 -0600

    global variable

--- advanced_rust/src/main.rs
+++ advanced_rust/src/main.rs
@@ -1,7 +1,5 @@
-unsafe extern "C" {
-    safe fn abs(input: i32) -> i32;
-}
+static HELLO_WORLD: &str = "Hello, world!";
 
 fn main() {
-    println!("Absolute value of -3 according to C: {}", abs(-3));
+    println!("name is: {HELLO_WORLD}");
 }
