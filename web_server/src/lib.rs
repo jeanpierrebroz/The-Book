a1539a565ad25babccc4653962ffb39f9d68ce3d
jeanpierrebroz <jeanpierrebroz@gmail.com>
2026-03-30 13:05:26 -0600

    store threads

--- web_server/src/lib.rs
+++ web_server/src/lib.rs
@@ -1,8 +1,27 @@
-pub struct ThreadPool;
+use std::thread;
+
+pub struct ThreadPool {
+    threads: Vec<thread::JoinHandle<()>>,
+}
 
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
-        ThreadPool
+        /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
+        assert!(size > 0);
+
+        let mut threads = Vec::with_capacity(size);
+
+        for _ in 0..size {
+            // create some threads and store them in the vector
+        }
+
+        ThreadPool { threads }
     }
 
     pub fn execute<F>(&self, f: F) where F: FnOnce() + Send + 'static {
